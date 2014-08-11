98e8cdf5442c9c921abf6573beb3ab08229a35a2
Billie <billie0912@gmail.com>
2014-08-11 20:30:45 +1000

    Created a rails 4 bootstrap web application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlickrImage::Application.config.secret_key_base = '6e94e002d189b375c44ad68b5f498363cd393abf18a3afb6d52e620bcd351d7a936fee10d1aeea10bdbdf762ba2a73a6654f8dff265c2f1974777b8ca4df57d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlickrImage::Application.config.session_store :cookie_store, key: '_flickr_image_session'
